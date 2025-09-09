9f73a68d1cd37cb89140ee2cfba189d492d0d1ef
Aya8Merai <aya5312.merai@gmail.com>
2025-09-09 18:58:06 +0300

    chore: project setup and dependencies

---
+++ src/main.ts
@@ -0,0 +1,16 @@
+import { ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const app = await NestFactory.create(AppModule);
+
+  // Enable global validation pipe
+  app.useGlobalPipes(new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+
+  await app.listen(process.env.PORT ?? 3001);
+}
+bootstrap();
---
+++ src/users/users.controller.ts
@@ -0,0 +1,53 @@
+import {
+  Controller,
+  Get,
+  Post,
+  Body,
+  Param,
+  Delete,
+  Put,
+  Query,
+} from '@nestjs/common';
+import { CreateUserDto } from './dto/create-user.dto';
+import { UsersService } from './users.service';
+
+@Controller('users')
+export class UsersController {
+  constructor(private readonly usersService: UsersService) {}
+
+  // 1- create()
+  @Post()
+  async create(@Body() createUserDto: CreateUserDto) {
+    return this.usersService.create(createUserDto);
+  }
+
+  // 2- findAll()
+  @Get()
+  async findAll(
+    @Query('offset') offset: number = 0, // كانت 1 وخليتها 0 حتى ما يطير واحد من المستخدمين
+    @Query('limit') limit: number = 10,
+  ) {
+    return this.usersService.findAll(offset, limit);
+  }
+
+  // 3- findOne()
+  @Get(':id')
+  async findOne(@Param('id') id: number) {
+    return this.usersService.findOne(id);
+  }
+
+  // 4- update()
+  @Put(':id')
+  async update(
+    @Param('id') id: number,
+    @Body() updateUserDto: Partial<CreateUserDto>,
+  ) {
+    return this.usersService.update(id, updateUserDto);
+  }
+
+  // 5- remove()
+  @Delete(':id')
+  async remove(@Param('id') id: number) {
+    return this.usersService.remove(id);
+  }
+}
