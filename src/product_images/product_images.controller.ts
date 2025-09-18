557fbcd63d2e5d8f7f142f7a6cbee2c90bf115c2
Aya8Merai <aya5312.merai@gmail.com>
2025-09-18 03:04:34 +0300

    fix: add the missing import for product_images auth

--- src/product_images/product_images.controller.ts
+++ src/product_images/product_images.controller.ts
@@ -14,6 +14,7 @@ import {
   DefaultValuePipe,
   Query,
   Body,
+  UseGuards,
 } from '@nestjs/common';
 import { FileInterceptor } from '@nestjs/platform-express';
 import { diskStorage } from 'multer';
